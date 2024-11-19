83001c2e9622698211556adfa4afa5b5c5aa38aa
Verma Abhishek <abhishek.vermax@gmail.com>
2024-11-19 15:53:42 +0100

    added all CRUD operations

--- src/main/firebase_firestore/firebase_crud_single.py
+++ src/main/firebase_firestore/firebase_crud_single.py
@@ -3,6 +3,7 @@ from src.main.firebase_firestore.firebase_connect import (
 )
 
 
+# Read operation on collection using collection name
 def read_document_per_collection_dict(cred_json_path, collection_name) -> dict:
     docs_collection = []
     db_client = get_firebase_fire_store_db_client(cred_json_path)
@@ -13,16 +14,38 @@ def read_document_per_collection_dict(cred_json_path, collection_name) -> dict:
     return docs_collection
 
 
-def create_document_in_collection(cred_json_path, collection_name, document_id,updates_dict):
+# Create operation on collection using document ID
+def create_document_in_collection(
+    cred_json_path, collection_name, document_id, create_doc_dict
+):
     db_client = get_firebase_fire_store_db_client(cred_json_path)
     doc_ref = db_client.collection(collection_name).document(document_id)
     # set object and push to firebase
-    doc_ref.set(updates_dict)
-    return f"Updates done on : {collection_name}, with document id: {document_id}"
-    
+    doc_ref.set(create_doc_dict)
+    print(f"Document created in : {collection_name}, with document id: {document_id}")
 
 
-def get_documentBy_id(cred_json_path, collection_name, document_id):
+# Delete operation on collection using document ID
+def delete_document_in_collection(cred_json_path, collection_name, document_id):
     db_client = get_firebase_fire_store_db_client(cred_json_path)
-    db_client.collection(collection_name).doc(document_id).get()
+    doc_ref = db_client.collection(collection_name).document(document_id).get()
+    if doc_ref.exists:
+        print(f"Document data that will be deleted: {doc_ref.to_dict()}")
+        # delete document id in collection if it exists
+        doc_ref.reference.delete()
+        print(f"Deletion done on : {collection_name}, for document id: {document_id}")
+    else:
+        print(f"No such document present in {collection_name}!")
 
+
+# Update operation on collection using document ID
+def update_document_in_collection(cred_json_path, collection_name, document_id,update_field_dict):
+    db_client = get_firebase_fire_store_db_client(cred_json_path)
+    doc_ref = db_client.collection(collection_name).document(document_id).get()
+    if doc_ref.exists:
+        print(f"Document data that will be deleted: {doc_ref.to_dict()}")
+        # delete document id in collection if it exists
+        doc_ref.reference.update(update_field_dict)
+        print(f"Updates done on : {collection_name}, for document id: {document_id}")
+    else:
+        print(f"No such document present in {collection_name}!")
--- src/test/firebase_firestore/test_firebase_crud_single.py
+++ src/test/firebase_firestore/test_firebase_crud_single.py
@@ -1,7 +1,9 @@
 import unittest
 from src.main.firebase_firestore.firebase_crud_single import (
     create_document_in_collection,
+    delete_document_in_collection,
     read_document_per_collection_dict,
+    update_document_in_collection,
 )
 
 
@@ -16,13 +18,58 @@ class TestFirebaseCRUDSingle(unittest.TestCase):
 
     def test_create_document_in_collection_dict(self):
         create_document_in_collection(
-            cred_json_path=self.cred_json, collection_name="category",document_id="new_doc_id",
-            updates_dict={"doc_id": "new_doc_id", "imageUrl": "sample_png", "name": "extra"}
+            cred_json_path=self.cred_json,
+            collection_name="category",
+            document_id="new_doc_id",
+            create_doc_dict={
+                "doc_id": "new_doc_id",
+                "imageUrl": "sample_png",
+                "name": "extra",
+            },
         )
         read_after_creation = read_document_per_collection_dict(
             cred_json_path=self.cred_json, collection_name="category"
         )
         self.assertTrue(len(read_after_creation), 5)
+
+    def test_delete_document_in_collection(self):
+        delete_document_in_collection(
+            cred_json_path=self.cred_json,
+            collection_name="category",
+            document_id="new_doc_id"
+        )
+        read_after_deletion = read_document_per_collection_dict(
+            cred_json_path=self.cred_json, collection_name="category"
+        )
+        self.assertTrue(len(read_after_deletion), 4)
+
+
+    def test_update_document_in_collection(self):
+        create_document_in_collection(
+            cred_json_path=self.cred_json,
+            collection_name="category",
+            document_id="created_to_test_updates_doc_id",
+            create_doc_dict={
+                "doc_id": "new_doc_id",
+                "imageUrl": "sample_png",
+                "name": "extra",
+            },
+        )
+        update_document_in_collection(
+            cred_json_path=self.cred_json,
+            collection_name="category",
+            document_id="created_to_test_updates_doc_id",update_field_dict={
+                "doc_id": "updated_doc_id"}
+        )
+        read_after_updation = read_document_per_collection_dict(
+            cred_json_path=self.cred_json, collection_name="category"
+        )
+        self.assertTrue(len(read_after_updation), 5)
+        delete_document_in_collection(
+            cred_json_path=self.cred_json,
+            collection_name="category",
+            document_id="created_to_test_updates_doc_id"
+        )
 
 
 # doc_ref.set({"doc_id": document_id, "imageUrl": "sample_png", "name": "extra"})
